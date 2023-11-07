25645cf763d0352b8f64164a19afe7c0cb9bbb4d
Abhimauryaa <110964150+Abhimauryaa@users.noreply.github.com>
2023-11-07 03:18:46 -0800

    Add files via upload

---
+++ reactjs_Assignment/state/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ClassState from './State/ClassState'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <ClassState/>
+        
+    </div>
+  )
+}
+
+export default App
